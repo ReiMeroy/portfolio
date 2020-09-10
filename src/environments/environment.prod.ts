3a6d9962d20a4122f304e1ddc1720c602178f928
ReiMeroy <xvwlllwvx@gmail.com>
2020-09-11 04:04:30 +0900

    feat:config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDcfNzzXOwZJqXVmlP_rdF3GB1LMp6wP7Y',
+    authDomain: 'portfolio-72126.firebaseapp.com',
+    databaseURL: 'https://portfolio-72126.firebaseio.com',
+    projectId: 'portfolio-72126',
+    storageBucket: 'portfolio-72126.appspot.com',
+    messagingSenderId: '181795753572',
+    appId: '1:181795753572:web:1d77edd06620c5fffd1971',
+    measurementId: 'G-0FVMWYC252',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDcfNzzXOwZJqXVmlP_rdF3GB1LMp6wP7Y',
+    authDomain: 'portfolio-72126.firebaseapp.com',
+    databaseURL: 'https://portfolio-72126.firebaseio.com',
+    projectId: 'portfolio-72126',
+    storageBucket: 'portfolio-72126.appspot.com',
+    messagingSenderId: '181795753572',
+    appId: '1:181795753572:web:1d77edd06620c5fffd1971',
+    measurementId: 'G-0FVMWYC252',
+  },
 };
 
 /*
